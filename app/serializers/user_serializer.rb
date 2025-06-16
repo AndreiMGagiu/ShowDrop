61e9e35c12e17e2e9c556e24d8f7ff27258e9632
AndreiMGagiu <gagiumandrei@gmail.com>
2025-06-16 09:27:00 +0300

    chore: add yard docs for user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,7 +1,15 @@
+# frozen_string_literal: true
+
+# UserSerializer serializes user data into a JSON:API-compliant format.
 class UserSerializer
   include JSONAPI::Serializer
+
+  # @return [Integer] the unique ID of the user
+  # @return [String] the user's email address
+  # @return [DateTime] the datetime when the user was created
   attributes :id, :email, :created_at
 
+  # @return [String, nil] the creation date in MM/DD/YYYY format
   attribute :created_date do |user|
     user.created_at&.strftime('%m/%d/%Y')
   end
